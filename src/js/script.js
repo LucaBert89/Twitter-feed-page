b235abba3157dd8d69e1969ec5d20667c917d7ca
LucaBert89 <62511269+LucaBert89@users.noreply.github.com>
2021-04-26 23:15:26 +0200

    fetch-user-tweet-added

--- src/js/script.js
+++ src/js/script.js
@@ -45,11 +45,15 @@ import "../main.scss";
     searchBtn.addEventListener("click", searchAccount);
 })();
 
+
 function searchAccount(e) {
-    let url =  `https://api.twitter.com/2/tweets/search/recent?query=nyc`;
-          fetch(url,  {
-            "method": "GET",
-            "headers": {
+    let search = document.querySelector(".search-container__input-text");
+    console.log(search.value);
+    //let url =  `https://api.twitter.com/2/users/23083404/tweets?media.fields=media_key,preview_image_url,url&expansions=attachments.media_keys,referenced_tweets.id`;
+          fetch(`https://api.twitter.com/2/users/by?usernames=${search.value}`,  {
+            method: "get",
+            headers: {
+                'Content-Type': 'application/json',
                 "Authorization": `Bearer ${process.env.Bearer_TOKEN}`,
             }
         })  
@@ -57,7 +61,64 @@ function searchAccount(e) {
           return res.json();
       })
       .then(body => {
-          console.log(body)
-      })
+         return body.data[0].id; 
+        }).then(data => {
+            getTweet(data);
+        })
+
+         
+}
+
+function getTweet(id) {
+    fetch(`https://api.twitter.com/2/users/${id}/tweets?media.fields=media_key,preview_image_url,url&tweet.fields=referenced_tweets,created_at&expansions=attachments.media_keys`,  {
+        method: "get",
+        headers: {
+            'Content-Type': 'application/json',
+            "Authorization": `Bearer ${process.env.Bearer_TOKEN}`,
+        }
+    }).then(res => {
+        return res.json();
+    })
+    .then(body => {
+        let Tweet;
+        console.log(body);
+        body.data.forEach(element => {
+            let TweetFeed = new Tweet(element.created_at, element.text, element.attachments.media_keys);
+            Tweet = document.createElement("div");
+            Tweet.classList.add("Tweet-feed__Tweet");
+            Tweet.appendChild(document.querySelector(".Tweet-feed"));
+            
+        })
+        /*body.data.forEach(element => {
+            body.includes.media.forEach(e => {
+                if (element.attachments.media_keys === e) {
+                    let 
+                }
+            })
+            
+            
+        });*/
+    })
+}
+
+
+class Tweet {
+    constructor(day, text, media) {
+        this.day = day;
+        this.text = text;
+        this.media = media;
+      }
+    
+    get displayData() {
+        return this.day;
+    }
+
+    get displayText() {
+        return this.text;
+    }
+
+    get displayMedia() {
+        if(this.media != undefined) return this.media;
+    }
 }
 
